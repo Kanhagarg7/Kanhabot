72c7afdc971c8fad2a3a206535acf112faefa791
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2024-07-30 12:53:55 +0530

    Update __main__.py

--- pykanha/__main__.py
+++ pykanha/__main__.py
@@ -1,112 +1,54 @@
-# kanha - UserBot
-# Copyright (C) 2021-2023 Teamkanha
-#
-# This file is a part of < https://github.com/Teamkanha/kanha/ >
-# PLease read the GNU Affero General Public License in
-# <https://github.com/Teamkanha/pykanha/blob/main/LICENSE>.
+from pyrogram import idle
+import dns.resolver
+import socket
 
-from . import *
+from Hellbot import __version__
+from Hellbot.core import (
+    Config,
+    ForcesubSetup,
+    GachaBotsSetup,
+    TemplateSetup,
+    UserSetup,
+    db,
+    hellbot,
+)
+from Hellbot.functions.tools import initialize_git
+from Hellbot.functions.utility import BList, Flood, TGraph
+
+def custom_resolver(hostname):
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = ["8.8.8.8", "8.8.4.4"]  # Using Google's DNS servers
+    answers = resolver.resolve(hostname, "A")
+    return answers[0].address
 
 
-def main():
-    import os
-    import sys
-    import time
+original_getaddrinfo = socket.getaddrinfo
 
-    from .fns.helper import bash, time_formatter, updater
-    from .startup.funcs import (
-        WasItRestart,
-        autopilot,
-        customize,
-        fetch_ann,
-        plug,
-        ready,
-        startup_stuff,
-    )
-    from .startup.loader import load_other_plugins
 
+def custom_getaddrinfo(host, port, family=0, type=0, proto=0, flags=0):
     try:
-        from apscheduler.schedulers.asyncio import AsyncIOScheduler
-    except ImportError:
-        AsyncIOScheduler = None
+        ip = custom_resolver(host)
+        return [(socket.AF_INET, socket.SOCK_STREAM, proto, "", (ip, port))]
+    except Exception as e:
+        return original_getaddrinfo(host, port, family, type, proto, flags)
 
-    # Option to Auto Update On Restarts..
-    if (
-        udB.get_key("UPDATE_ON_RESTART")
-        and os.path.exists(".git")
-        and kanha_bot.run_in_loop(updater())
-    ):
-        kanha_bot.run_in_loop(bash("bash installer.sh"))
 
-        os.execl(sys.executable, sys.executable, "-m", "pykanha")
+socket.getaddrinfo = custom_getaddrinfo
 
-    kanha_bot.run_in_loop(startup_stuff())
-
-    kanha_bot.me.phone = None
-
-    if not kanha_bot.me.bot:
-        udB.set_key("OWNER_ID", kanha_bot.uid)
-
-    LOGS.info("Initialising...")
-
-    kanha_bot.run_in_loop(autopilot())
-
-    pmbot = udB.get_key("PMBOT")
-    manager = udB.get_key("MANAGER")
-    addons = udB.get_key("ADDONS") or Var.ADDONS
-    vcbot = udB.get_key("VCBOT") or Var.VCBOT
-    if HOSTED_ON == "okteto":
-        vcbot = False
-
-    if (HOSTED_ON == "termux" or udB.get_key("LITE_DEPLOY")) and udB.get_key(
-        "EXCLUDE_OFFICIAL"
-    ) is None:
-        _plugins = "autocorrect autopic audiotools compressor forcesubscribe fedutils gdrive glitch instagram nsfwfilter nightmode pdftools profanityfilter writer youtube"
-        udB.set_key("EXCLUDE_OFFICIAL", _plugins)
-
-    load_other_plugins(addons=addons, pmbot=pmbot, manager=manager, vcbot=vcbot)
-
-    suc_msg = """
-            ----------------------------------------------------------------------
-                kanha has been deployed! Visit @Thekanha for updates!!
-            ----------------------------------------------------------------------
-    """
-
-    # for channel plugins
-    plugin_channels = udB.get_key("PLUGIN_CHANNEL")
-
-    # Customize kanha Assistant...
-    kanha_bot.run_in_loop(customize())
-
-    # Load Addons from Plugin Channels.
-    if plugin_channels:
-        kanha_bot.run_in_loop(plug(plugin_channels))
-
-    # Send/Ignore Deploy Message..
-    if not udB.get_key("LOG_OFF"):
-        kanha_bot.run_in_loop(ready())
-
-    # TODO: Announcement API IS DOWN
-    # if AsyncIOScheduler:
-    #     scheduler = AsyncIOScheduler()
-    #     scheduler.add_job(fetch_ann, "interval", minutes=12 * 60)
-    #     scheduler.start()
-
-    # Edit Restarting Message (if It's restarting)
-    kanha_bot.run_in_loop(WasItRestart(udB))
-
-    try:
-        cleanup_cache()
-    except BaseException:
-        pass
-
-    LOGS.info(
-        f"Took {time_formatter((time.time() - start_time)*1000)} to start •ULTROID•"
-    )
-    LOGS.info(suc_msg)
+async def main():
+    await hellbot.startup()
+    await db.connect()
+    await UserSetup()
+    await ForcesubSetup()
+    await GachaBotsSetup()
+    await TemplateSetup()
+    await Flood.updateFromDB()
+    await BList.updateBlacklists()
+    await TGraph.setup()
+    await initialize_git(Config.PLUGINS_REPO)
+    await hellbot.start_message(__version__)
+    await idle()
 
 
 if __name__ == "__main__":
-    main()
-
-    asst.run()
+    hellbot.run(main())
